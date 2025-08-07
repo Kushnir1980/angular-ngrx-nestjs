be5dd0b0113c59a214523d066782ee18bbf775eb
Serhii Kushnir <kushnir.1980@gmail.com>
2025-08-07 17:24:28 +0300

    fix id

--- nest/src/products/product-sql-orm.repository.ts
+++ nest/src/products/product-sql-orm.repository.ts
@@ -10,24 +10,25 @@ export class SqLiteRepository {
     constructor(@InjectRepository(Product) private repo: Repository<Product>) { }
 
     async findAll() {
-
+        return this.repo.find();
     }
 
     async findById(id: string) {
-
+        return this.repo.findOneBy({ id });
     }
 
     async create(id: string, product: ProductDto) {
-        const productEntity = this.repo.create({ ...product } as Product); // Create a new Product entity
-        this.repo.save(productEntity); // Save to SqLite
+        const productEntity = this.repo.create(product as Product); // Create a new Product entity
+        return this.repo.save(productEntity); // Save to SqLite
     }
 
-    async update(id: string, product: ProductDto) {
-
+    async update(id: string, product: Partial<Product>) {
+        await this.repo.update(id, product); // Update the existing product
+        return this.findById(id); //
     }
 
     async remove(id: string) {
-
+        return this.repo.delete(id); // Remove the product by id
     }
 
 }
